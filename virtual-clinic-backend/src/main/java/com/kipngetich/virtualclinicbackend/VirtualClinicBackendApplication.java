13e0543cb9281e189b5ccd8e5adea210d7b73e81
Robert Cheruiyot <cheruiyot@ymail.com>
2021-01-25 10:59:57 -0600

    init commit

---
+++ virtual-clinic-backend/src/main/java/com/kipngetich/virtualclinicbackend/VirtualClinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kipngetich.virtualclinicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClinicBackendApplication.class, args);
+	}
+
+}
---
+++ virtual-clinic-backend/src/test/java/com/kipngetich/virtualclinicbackend/VirtualClinicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipngetich.virtualclinicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClinicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
